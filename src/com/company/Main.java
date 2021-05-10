bfe5e51f69187dc64d0eac9de47c72204d16402f
gizem <gizemsacal@gmail.com>
2021-05-10 16:14:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.EnumMap;
+
+class Main {
+
+    enum Size {
+        SMALL, MEDIUM, LARGE, EXTRALARGE
+    }
+    public static void main(String[] args) {
+
+        // Creating an EnumMap of the Size enum
+        EnumMap<Size, Integer> sizes1 = new EnumMap<>(Size.class);
+
+        // Using the put() Method
+        sizes1.put(Size.SMALL, 28);
+        sizes1.put(Size.MEDIUM, 32);
+        System.out.println("EnumMap1: " + sizes1);
+
+        EnumMap<Size, Integer> sizes2 = new EnumMap<>(Size.class);
+
+        // Using the putAll() Method
+        sizes2.putAll(sizes1);
+        sizes2.put(Size.LARGE, 36);
+        System.out.println("EnumMap2: " + sizes2);
+    }
+    /*Yukarıdaki örnekte, putAll()bir enum haritasının
+    tüm öğelerini eklemek için yöntemi kullandık*/
+}
